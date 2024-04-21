3a1efbfb38e7c707c5debe0f48aa7609c41f2c64
Ahnibaba <aniebietedet2025@gmail.com>
2024-04-21 10:41:48 +0100

    first-commit

---
+++ src/features/posts/ReactionButtons.js
@@ -0,0 +1,37 @@
+import { useDispatch } from "react-redux";
+import { reactionAdded } from "./postsSlice";
+
+const reactionEmoji = {
+    thumbsUp: "👍",
+    wow: "😮",
+    heart: "💗",
+    rocket: "🚀",
+    coffee: "☕"
+}
+
+
+
+const ReactionButtons = ({ post }) => {
+    const dispatch = useDispatch();
+
+    const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
+        
+        const reactionCount = post.reactions ? post.reactions[name] || 0 : 0;
+       return (
+        <button 
+           key={name}
+           type="button"
+           className="reactionButton"
+           onClick={() =>
+              dispatch(reactionAdded({ postId: post.id, reaction: name}))
+           }
+           
+        >
+          {emoji} {reactionCount}
+        </button>
+       )
+    })
+  return <div>{reactionButtons}</div>
+}
+
+export default ReactionButtons
